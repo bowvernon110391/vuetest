3c2c0acd6d1d2364d065f96515150628c327a497
PDAD Soetta <pdad.soetta@gmail.com>
2019-10-21 18:32:22 +0700

    Initial commit

---
+++ processing.js
@@ -0,0 +1,5 @@
+jQuery.fn.dataTable.Api.register( 'processing()', function ( show ) {
+    return this.iterator( 'table', function ( ctx ) {
+        ctx.oApi._fnProcessingDisplay( ctx, show );
+    } );
+} );
